3ad1ddae5bac39a4401637ca5431a483b46c06fd
BRANDON PARKER <Brandon@dkparker.com>
2016-03-29 14:01:05 -0700

    tested increments for grabbing specific data, now to define other variables for later days

--- app/scripts/dataGrab.js
+++ app/scripts/dataGrab.js
@@ -1,71 +1,42 @@
 // $(function(module) {
 //   var beach = {};
-//
-//
-//   // beach.callData = function() {
-//   //   var jsonUrl = 'http://tidesandcurrents.noaa.gov/api/datagetter?callback=?';
-//   //   $.getJSON(jsonUrl, {
-//   //     begin_date: '20130808 08:00',
-//   //     end_date: '20130808 09:00',
-//   //     station: '9447130',
-//   //     datum: 'MTL',
-//   //     product: 'water_temperature',
-//   //     units: 'english',
-//   //     time_zone: 'gmt',
-//   //     interval: 'h',
-//   //     format: 'json'
-//   //   }).always(function(data) {
-//   //     console.log('done', data);
-//   //   }).fail(function(e) {
-//   //     console.log('this is error', e);
-//   //   });
-//   //
-//   //
-//   // };
-//
-//   // $.ajax({ url:'https://www.ncdc.noaa.gov/cdo-web/api/v2/location',
-//   //   headers:{ token:'zOnNnVAWTPfuwrgvemgqLBuIRZCewZpT' }
-//   //   })
-//   //   .done(function(data,message,xhr) {
-//   //     console.log(data)
-//   // });
-//
+  var callbackString = function(e) {
+    JSON.stringify(e);
+  };
+
+  var callData = function() {
+    var jsonUrl = '/tides/';
+    $.get(jsonUrl, {
+      // end_date: '20120331',
+      // range: 48,
+      // interval: 'h',
+      date: 'today',
+      // date: 'latest',
+      // range: 12,
+      // begin_date: '20130808 08:00',
+      // end_date: '20130808 09:00',
+      station: '9447130',
+      datum: 'MTL',
+      product: 'predictions',
+      units: 'english',
+      time_zone: 'gmt',
+      format: 'json'
+    }).done(function(data) {
+      console.log('done', data);
+    }).fail(function(e) {
+      console.log('this is error', e);
+    });
+
+  };
+
+  var dataToday
+  // Need to find next Low Tide, 12 hour increments
+
+  var dataTomorrow
+  // Need to find next Low Tide, 12 hour increments
+
+  var dataTwoDays
+  // Need to find next Low Tide, 12 hour increments
+
 //   module.beach = beach;
 // })(window);
-//
-// // $.ajax({ url:'https://tidesandcurrents.noaa.gov/api/datagetter' +
-// // '?begin_date=20130808%2015:00&end_date=20130808%2015:06&station=9447130&datum=MTL' +
-// // '&product=water_temperature&units=english&time_zone=gmt' +
-// // '&application=ports_screen&format=json',
-// //   headers:{ token:'zOnNnVAWTPfuwrgvemgqLBuIRZCewZpT' }
-// //   })
-// //   .done(function(data,message,xhr) {
-// //     console.log(data)
-// // });
-//
-//
-//
-// // {'metadata':{'id':'9447130','name':'Seattle','lat':'47.6026','lon':'-122.3393'}, 'data': [{'t':'2013-08-08 08:00', 'v':'57.4', 'f':'0,0,0'},{'t':'2013-08-08 09:00','v':'56.5', 'f':'0,0,0'}]}
-var callbackString = function(e){
-  JSON.stringify(e);
-};
-
-var callData = function() {
-  var jsonUrl = '/tides/';
-  $.get(jsonUrl, {
-    begin_date: '20130808 08:00',
-    end_date: '20130808 09:00',
-    station: '9447130',
-    datum: 'MTL',
-    product: 'predictions',
-    units: 'english',
-    time_zone: 'gmt',
-    format: 'json'
-  }).done(function(data) {
-    console.log('done', data);
-  }).fail(function(e) {
-    console.log('this is error', e);
-  });
-
-
-};
