38650e67b40b45cb31dd6687212403a8cd9565cf
BRANDON PARKER <Brandon@dkparker.com>
2016-03-29 15:49:32 -0700

    added functionality to add new object to Firebase

--- app/scripts/locationsView.js
+++ app/scripts/locationsView.js
@@ -1,10 +1,21 @@
+var ref = new Firebase('https://ebb-tracker.firebaseio.com/');
+
 var beachData = {};
 var beachArray = [];
 var keys = [];
 
-var ref = new Firebase('https://ebb-tracker.firebaseio.com/');
 
 ref.on('value', function(snapshot) {
   console.log(snapshot.val());
   beachArray = snapshot.val().beaches;
 });
+
+var beachRef = ref.child('beaches');
+  beachRef.push({
+    'exampleUpdate': {'name': 'Cark Par',
+      'longitude': '-122.372298',
+      'latitude': '47.712489',
+      'stationID': '9447130',
+      'url': 'http://www.seattle.gov/parks/environment/carkeek.htm'
+    }
+  });
