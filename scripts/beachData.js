8a3a5bf46fef437ecca7c402d22f03460e50f270
bonecrushereb <bonecrushereb@gmail.com>
2016-03-31 21:15:31 -0700

    removed console.log and commented code from beachData and mainController

--- scripts/beachData.js
+++ scripts/beachData.js
@@ -5,15 +5,7 @@
 
   var ref = new Firebase('https://ebb-tracker.firebaseio.com/beaches');
 
-
-  // var sync = $firebase(ref.orderByChild("name").equalTo('Carkeek Park'));
-
-  // ref.orderByChild("exampleUpdate").on("value", function(snapshot) {
-  //   console.log(snapshot.key() + " was ", snapshot.val().child("exampleUpdate")," meters tall");
-  // });
-
   beachData.retrieveData = function(ctx, next) {
-    console.log('retrieve data fires');
     ref.once("value", function(snapshot) {
       // The callback function will get called twice, once for "fred" and once for "barney"
       if (beachData.beachArray.length > 0){
@@ -22,34 +14,13 @@
       snapshot.forEach(function(childSnapshot) {
         // key will be "fred" the first time and "barney" the second time
         var key = childSnapshot.key();
-        console.log('key', key);
         // childData will be the actual contents of the child
         var childData = childSnapshot.val();
-        console.log('childData', childData);
         beachData.beachArray.push(childData.exampleUpdate);
       });
-      console.log(beachData.beachArray);
       next();
     });
-  }
-
-  // var beaches = ref.orderByChild('exampleUpdate');
-  //
-  // ref.on('value', function(snapshot) {
-  //   console.log('log value',snapshot.val());
-  //   console.log('log key',snapshot.key());
-  //   beachArray = snapshot.val().beaches;
-  // });
-
-  // var beachRef = ref.child('beaches');
-  //   beachRef.push({
-  //     'exampleUpdate': {'name': 'Cark Par',
-  //       'longitude': '-122.372298',
-  //       'latitude': '47.712489',
-  //       'stationID': '9447130',
-  //       'url': 'http://www.seattle.gov/parks/environment/carkeek.htm'
-  //     }
-  //   });
+  };
 
   module.beachData = beachData;
 })(window);
--- scripts/mainController.js
+++ scripts/mainController.js
@@ -3,14 +3,10 @@
     var clickedBeach = $(this).data('destination').replace(' ','-');
     var lng = $(this).data('lng');
     var lat = $(this).data('lat');
-    console.log('this has stuff',$(this).data('destination'));
     if(lng){
-      console.log('lng present');
       location.href = 'beach/'+clickedBeach+'/'+lng+'/'+lat;
     } else {
-      console.log('lng not present');
       location.href = 'beach/'+clickedBeach;
-      console.log(clickedBeach);
     }
   });
   var beaches = [];
@@ -27,20 +23,17 @@
   };
 
   Beach.renderSearchResults = function(ctx, next) {
-    console.log('renderSearchResults');
-    console.log('beaches', Beach.all);
+
     $('#index .result').empty();
     Beach.all.forEach(function(ele) {
       ele.userLng = mainView.userLng;
       ele.userLat = mainView.userLat;
-      console.log('ele', ele);
       $('#index .result').append(ele.toHtml($('#beach-template')));
     });
   };
 
   Beach.renderLocations = function(ctx, next) {
-    console.log('renderLocations');
-    console.log('beachsArray',beaches)
+
     $('#location-result').empty();
     beaches.forEach(function(ele) {
       $('#location-result').append(ele.toHtml($('#map-template')));
@@ -49,10 +42,8 @@
   };
 
   Beach.renderDetailView = function(ctx, next) {
-    console.log('renderDetailView');
-    console.log('this is beaches', beaches);
+
     beaches.forEach(function(ele) {
-      console.log('ele', ele);
       $('#detail #map2').insertAfter('#detail');
       $('#detail').empty().append(ele.toHtml($('#detail-template')));
     });
@@ -61,8 +52,6 @@
 
   Beach.addTideData = function(ctx, next) {
 
-    console.log('beach', beaches[0]);
-    console.log('tide data', ctx.tideData);
     beaches[0].tideData = ctx.tideData;
     next();
 
@@ -73,25 +62,18 @@
 
     });
 
-    console.log('beach', beaches[0]);
-    console.log('tide data', ctx.tideData);
     beaches[0].tideData = ctx.tideData;
     next();
 
   };
   Beach.loadAll = function(ctx, next) {
     beaches = [];
-    console.log('this is beaches after i cleared array', beaches);
-    console.log('loadAll');
-    console.log('beachArray', beachData.beachArray);
+
     if (ctx.filterByName) {
-      console.log('filter by stayed', ctx.filterByName);
-      console.log('matching beach data', beachData.beachArray);
+
       beachData.beachArray.forEach(function(ele) {
-        console.log(ele.name);
         if (ele.name === ctx.filterByName) {
-          console.log(ele.name, ctx.filterByName);
-          console.log(ele.stationID);
+
           ctx.stationID = ele.stationID;
           ctx.lng = ele.longitude;
           ctx.lat = ele.latitude;
@@ -109,21 +91,11 @@
   Beach.loadSearchResults = function(callback) {
     Beach.all = [];
     $('#index .result').empty();
-    console.log('this is beaches after i cleared array', beaches);
-    console.log('loadAll');
-    console.log('beachArray', beachData.beachArray);
 
     indexController.closeBeaches.forEach(function(ele, idx, arr) {
       var stationID = ele.stationID;
-      console.log(stationID, ele.name);
       tideData.nextLowTideData(stationID, idx,ele,callback);
     });
-    // indexController.closeBeaches.forEach(function(ele, idx, arr) {
-    //
-    //   Beach.all.push(new Beach(ele));
-    // });
-
-
   };
 
   module.Beach = Beach;
