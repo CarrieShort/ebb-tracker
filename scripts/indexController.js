5446ec98e5578a332f472ac7135970a0f94cf409
Kate Lucas <katherinelucas@gmail.com>
2016-03-31 21:11:08 -0700

    added longitude range in indexController, cleaned up consolelogs

--- scripts/indexController.js
+++ scripts/indexController.js
@@ -5,32 +5,18 @@
   indexController.generateCloseBeaches = function() {
     indexController.closeBeaches = beachData.beachArray.filter(function(ele) {
       var parkLat = (parseFloat(ele.latitude) * 1000000000);
-      console.log(parseFloat(ele.latitude));
       var parkLng = (parseFloat(ele.longitude) * 1000000000);
-      console.log('parkLat', parkLat);
 
       var range = parseFloat(110000000);
-      console.log(ele.latitude, ele.longitude, ele.name);
-      console.log('ddfdf', mainView.userLat, mainView.userLng);
-
       var userLowerLat = mainView.userLat * 1000000000 - range;
-      console.log(userLowerLat);
-
       var userUpperLat = mainView.userLat * 1000000000 + range;
-      console.log(userUpperLat);
-
       var userLowerLng = mainView.userLng * 1000000000 - range;
-      console.log(userLowerLng);
-
       var userUpperLng = mainView.userLng * 1000000000 + range;
-      console.log(userUpperLng);
 
-
-      console.log(parkLat > userLowerLat && parkLat < userUpperLat);
-      return parkLat > userLowerLat && parkLat < userUpperLat;
+      return (parkLat > userLowerLat && parkLat < userUpperLat), (parkLng > userLowerLng && parkLng < userUpperLng);
     });
   };
 
-  
+
   module.indexController = indexController;
 })(window);
